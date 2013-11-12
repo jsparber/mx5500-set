369ad5f88dcc1f36a8c840aff0aeddc9477a0ef7
Julian Sparber <julian@sparber.net>
2013-11-12 23:19:41 +0100

    add some functions and test functions

--- mx5500.c
+++ mx5500.c
@@ -32,13 +32,20 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <math.h>
 int open_device(int fd);
 const char *bus_str(int bus);
 int set_time(int fd);
 int beep(int fd);
-int set_temp_unit(int fd, char unit);
+int set_temp_unit(int fd, char unit, char mode);
+int set_time_mode(int fd, char mode);
+int set_name(int fd);
+int set_kbd_opts(int fd, char com);
+int set_icons(int fd);
+int get_battery_status(int fd);
 int no_arg();
 int check(int res);
+int calc_dec_val(char buf[]);
 
 int main(int argc, char *argv[]){
 	int fd;
@@ -49,6 +56,21 @@ int main(int argc, char *argv[]){
 	if (!open_device(fd)){
 		if ( argc > 1 ){
 			switch (*(argv[1]+1)){
+				case 'i':
+						printf("Set icons...\n");
+						set_icons(fd);
+						printf("Done.\n");
+						break;
+				case 'k':
+						printf("Send settings...\n");
+						set_kbd_opts(fd, ' ');
+						printf("Done.\n");
+						break;
+				case 'n':
+						printf("Set name...\n");
+						set_name(fd);
+						printf("Done.\n");
+						break;
 				case 't':
 						printf("Set time...\n");
 						set_time(fd);
@@ -60,9 +82,11 @@ int main(int argc, char *argv[]){
 						printf("Done.\n");
 						break;
 				case 'u':
-						printf("Set temp_unit...\n");
-						if ( argc > 2 && (*argv[2] == 'c' || *argv[2] == 'f' )){
-							set_temp_unit(fd, *(argv[2]));
+						printf("Set Temp Unit and Time Mode...\n");
+						if ( argc > 3 &&
+						(*argv[2] == 'c' || *argv[2] == 'f' ) &&
+						(*argv[3] == 'h' || *argv[3] == 'H' )){
+							set_temp_unit(fd, *(argv[2]), *(argv[3]));
 							printf("Done.\n");
 						}
 						else{
@@ -77,12 +101,61 @@ int main(int argc, char *argv[]){
 		else {
 			no_arg();
 		}
+		get_battery_status(fd);
 		close(fd);
 	}
 	else
 		no_arg();
 	return 0;
 }
+
+
+      /* Get a report from the device */
+int get_battery_status(int fd){
+
+	const char get_batt[] = { 0x10, 0x01, 0x80, 0x00, 0x02, 0x00, 0x00 };
+	char buf[20];
+	int i;
+	int j=0;
+	int res;
+	res = write(fd, get_batt, 7);
+	printf("Specizal ops\n");
+	while (j < 100){
+	res = read(fd, buf, 20);
+	if (res < 0) {
+		perror("read");
+	}
+	else {
+		printf("read() read %d bytes:\n\t", res);
+		for (i = 0; i < res; i++)
+		printf("%hhx ", buf[i]);
+		puts("\n");
+	}
+	j++;
+	if (buf[2] == 10 )
+		calc_dec_val(buf);
+	}
+	
+	return 0;
+}
+int calc_dec_val(char buf[]){
+	int res = 0;
+	int i = 17;
+	int j = 0;
+	while( i > 2 && (buf[i] != 32 || buf[i] != 45)){
+		
+		if ( buf[i] != 46){
+			//res = ((buf[i] - 48) * pow( 16, j ));
+			j++;
+			i--;
+		printf("This is Data is %d\n", res);
+		i = -1;
+		}
+		else
+		i++;
+	}
+	return 0;
+} 
 
 /* Send a Report to the Device */
 /* Report Number is always the first element*/
@@ -104,18 +177,24 @@ int beep(int fd){
 
 
 /*changes temp unit*/
-int set_temp_unit(int fd, char unit){
+int set_temp_unit(int fd, char unit, char mode){
 	int res;
 	/*char tempunit1[] = { 0x10, 0x01, 0x81, 0x30, 0x00, 0x00, 0x00 };*/
-	char tempunit2[] = { 0x10, 0x01, 0x80, 0x30, 0xDC, 0x00, 0x00 };
+	char time_temp[] = { 0x10, 0x01, 0x80, 0x30, 0xDC, 0x00, 0x00 };
 
 	/* 0x00 for °C and 0x01 for °F*/
 	if ( unit != 'f' )
-		tempunit2[6] = 0x00;
+		time_temp[6] = 0x00;
 	else
-		tempunit2[6] = 0x01;
+		time_temp[6] = 0x01;
+	
+	/* 0xDC for 24h and 0xDB for 12h*/
+	if ( mode != 'h' )
+		time_temp[4] = 0xDC;
+	else
+		time_temp[4] = 0xDB;
 		
-	res = write(fd, tempunit2, 7);
+	res = write(fd, time_temp, 7);
 	if (res < 0) {
 		printf("Error: %d\n", errno);
 		perror("write");
@@ -155,21 +234,78 @@ int set_time(int fd){
   	return res;
 }
 
-      /* Get a report from the device */
-      /*  
-        res = read(fd, buf, 16);
-        if (res < 0) {
-                perror("read");
-        } else {
-                printf("read() read %d bytes:\n\t", res);
-                for (i = 0; i < res; i++)
-                        printf("%hhx ", buf[i]);
-                puts("\n");
-        }*/
+int set_name(int fd){
+	/*void mx5000_set_name(int fd, char buf[14], int len)
+	{
+  char line2[19] = { 0x01, 0x82, 0x34, 0x04, 0x01, 
+		     0x00, 0x00, 0x00, 0x00, 0x00, 
+		     0x00, 0x00, 0x80, 0x00, 0x00, 
+		     0x00, 0xFB, 0x12, 0x00 };
+  
+  if (len < 0)
+    len = strlen(buf);
 
+  if (len > 11)
+    len = 11;
+
+  line2[3] = len+1;
+
+  memcpy(line2+5, buf, len);
+
+
+
+  mx5000_send_report(fd, line2, 0x11);*/
+	int res;
+	int len = -1;
+	char buf[] = "Julian";
+	char line2[20] = {	0x11, 0x01, 0x82, 0x34, 0x04, 0x01, 
+						0x00, 0x00, 0x00, 0x00, 0x00, 
+						0x00, 0x00, 0x80, 0x00, 0x00, 
+						0x00, 0xFB, 0x12, 0x00 };
+	if (len < 0)
+		len = strlen(buf);
+
+	if (len > 11)
+		len = 11;
+	printf("Lang=%d\n",len);
+	line2[4] = len+1;
+	/*printf("Lang=%d\n",len);
+	memcpy(line2+6, buf, len);*/
+	res = write(fd, line2, 20);
+	return res;
+
+}
+
+int set_icons(int fd){
+	char icons[] = { 0x11, 0x01, 0x82, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	/*ICON_OFF = 0x00,
+	ICON_ON = 0x01,
+	ICON_BLINK = 0x02*/
+	/*email*/
+	icons[3] = 0x02;
+	/*icons[4] = messenger;
+	icons[5] = mute;
+	icons[7] = walkie;*/
+	write(fd, icons, 20);
+
+	return 0;
+}
+int set_kbd_opts (int fd, char com){
+	int res;
+	const char keyopts1[] = { 0x10, 0x01, 0x81, 0x01, 0x00, 0x00, 0x00 };
+	char keyopts2[] =       { 0x10, 0x01, 0x80, 0x01, 0x14, 0x00, 0x00 };
+	/*ENABLE_EVERYTHING = 0x00,
+  	DISABLE_BEEP_ON_SPECIAL_KEYS = 0x01,
+  	DISABLE_MEDIA_KEYS = 0x02,*/
+	keyopts2[6] = 0x02;
+	res = write(fd, keyopts1, 7);
+	res += write(fd, keyopts2, 7);
+
+	return res;
+}
 
 int check(int res){
-	
+	return 0;
 }
 
 /*Open the device*/
@@ -240,6 +376,6 @@ const char *bus_str(int bus){
 }
 
 int no_arg(){
-	printf("No Operation argument\nArguments:\n\t-b -> for beep\n\t-t -> sets time to localtime\n\t-u -> set temp unit -u < c | f >\n");
+	printf("No Operation argument\n	Arguments:\n\t-b -> for beep\n\t-t -> sets time to localtime\n\t-u -> set Temp Unit (<f> or <c>) and Time Mode (<h> or <H>)\n\tExample: -u c H \n");
 	return 0;
 }
